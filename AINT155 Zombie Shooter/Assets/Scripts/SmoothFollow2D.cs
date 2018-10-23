1bf0ad56dd748ed55c51d2c4319a4de2ff7908f5
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2018-10-23 16:34:31 +0100

    1c and 1d completed
    Basic gameplay now working

---
+++ AINT155 Zombie Shooter/Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow2D : MonoBehaviour {
+    public Transform target;
+    public float smoothing = 5.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * 0.001f));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- AINT155 Zombie Shooter/Assets/Scripts/TopDownCharacterController2D.cs
+++ AINT155 Zombie Shooter/Assets/Scripts/TopDownCharacterController2D.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownCharacterController2D : MonoBehaviour {
-
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
 	// Use this for initialization
 	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void FixedUpdate() // This is used in Physics instead of using the update code.
+    {
+        float x = Input.GetAxis("Horizontal"); //Takes user's input of A/D or LEFT/RIGHT keys
+        float y = Input.GetAxis("Vertical"); //Takes user's input of W/S or UP/DOWN keys
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+        
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
