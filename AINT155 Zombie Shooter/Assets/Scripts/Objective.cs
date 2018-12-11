a18f346ce6945234fd95835c1b3f0edb45acf29b
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2018-12-11 14:20:14 +0000

    Addition of new music

--- AINT155 Zombie Shooter/Assets/Scripts/Objective.cs
+++ AINT155 Zombie Shooter/Assets/Scripts/Objective.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class Objective : MonoBehaviour {
 
-    public int SpawnersToKill = 3;
+    public int SpawnersToKill = 4;
     public int Count = 0;
     public Text ObjectiveText;
     public void SpawnerDestroyed()
     {
         Count++; //increase count by one
         print(Count); //Debug
-        ObjectiveText.text = "Your Objective: \n Destroy all the spawners: " + Count.ToString() + "/3"; //Updates the objective text when the player destroys a spawner
+        ObjectiveText.text = "Your Objective: \n Destroy all the spawners: " + Count.ToString() + "/4"; //Updates the objective text when the player destroys a spawner
         if (Count >= SpawnersToKill)
         {
             SceneManager.LoadScene("Zombie Shooter Level 2"); //temporary, will load to next level at this point.
